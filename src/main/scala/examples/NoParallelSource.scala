132e1ebf786fe8acef45e2e5fc1ba7e9459d9978
yennanliu <f339339@gmail.com>
2020-10-18 16:08:27 +0800

    add ParallelSource, NoParallelSource

---
+++ src/main/scala/examples/NoParallelSource.scala
@@ -0,0 +1,22 @@
+package examples
+
+import org.apache.flink.streaming.api.functions.source.SourceFunction
+import org.apache.flink.streaming.api.functions.source.SourceFunction.SourceContext
+
+class NoParallelSource extends SourceFunction[Long]{
+
+  var count = 1L
+  var isRunning = true
+
+  override def run(ctx: SourceContext[Long]) = {
+    while (isRunning){
+      ctx.collect(count)
+      count += 1
+      Thread.sleep(1000)
+    }
+  }
+
+  override def cancel() = {
+    isRunning = false
+  }
+}
---
+++ src/main/scala/examples/ParallelSource.scala
@@ -0,0 +1,21 @@
+package examples
+
+import org.apache.flink.streaming.api.functions.source.ParallelSourceFunction
+import org.apache.flink.streaming.api.functions.source.SourceFunction.SourceContext
+
+class ParallelSource extends ParallelSourceFunction[Long]{
+    var count = 1L
+    var isRunning = true
+
+  override def run(ctx: SourceContext[Long]) = {
+    while (isRunning){
+      ctx.collect(count)
+      count += 1
+      Thread.sleep(1000)
+    }
+  }
+
+  override def cancel() = {
+    isRunning = false
+  }
+}
