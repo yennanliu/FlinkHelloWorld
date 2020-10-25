5656e70366304c531f8b3be904ab4d03a3ca792b
yennanliu <f339339@gmail.com>
2020-10-25 14:50:45 +0800

    add StreamDemoMyPartitioner.scala

---
+++ src/main/scala/examples/StreamDemoMyPartitioner.scala
@@ -0,0 +1,36 @@
+package examples
+
+import java.util
+
+import org.apache.flink.streaming.api.collector.selector.OutputSelector
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+// UDF
+import examples.ParallelSource
+
+object StreamDemoMyPartitioner extends App{
+
+  val env = StreamExecutionEnvironment.getExecutionEnvironment
+  env.setParallelism(2)
+
+  // implicit transformation
+  import org.apache.flink.api.scala._
+
+  val text = env.addSource(new ParallelSource)
+
+  // transform Long to Tuple dtype
+  val tupleData = text.map(line =>{
+    Tuple1(line)
+  })
+
+  val partitionData = tupleData.partitionCustom(new MyPartitioner, 0)
+
+  val result = partitionData.map(line => {
+    println("CURRENT PROCESS ID : " + Thread.currentThread().getId + " , value : " + line)
+    line._1
+  })
+
+  result.print().setParallelism(1)
+  env.execute("StreamDemoMyPartitioner")
+
+}
