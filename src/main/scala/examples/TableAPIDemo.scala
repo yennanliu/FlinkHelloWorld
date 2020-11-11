e42626d9e1568155b3b787a4a22416cbe92ce90a
yennanliu <f339339@gmail.com>
2020-11-11 23:53:47 +0800

    update backup code (TableAPIDemo.scala

--- src/main/scala/examples/TableAPIDemo.scala
+++ src/main/scala/examples/TableAPIDemo.scala
@@ -1,5 +1,8 @@
 package examples
 
+import java.sql.Types
+
+import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.streaming.api.collector.selector.OutputSelector
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 
@@ -15,9 +18,25 @@ object TableAPIDemo extends App{
 
   val env = StreamExecutionEnvironment.getExecutionEnvironment
 
+  // TODO : fix import errors, fix syntax errors
+
+  // 1. register a table via TableSource
+
   //val tableEnv = TableEnvironment.create(fsSettings)
 
   // val csvSource:TableSource = new CsvTableSource("/data/test.csv")
-  //
+
   // tableEnv.registerTableSource("CsvTable", csvSource)
-}
+
+  // 2. save data to outside source via TableSink
+
+  //  val tableEnv2 = TableEnvironment.getTableEnvironment(env)
+  //
+  //  val csvSink:Tablesink = new CsvTableSink("/output/output.csv")
+  //
+  //  val fieldName:Array[String] = Array("a", "b", "c")
+  //
+  //  val fieldType:Array[TypeInformation[_]] = Array(Types.INTEGER, Types.STING, Types.LONG)
+  //
+  //  tableEnv2.registerTableSink("csvTable", fieldName, fieldType, csvSink)
+  }
