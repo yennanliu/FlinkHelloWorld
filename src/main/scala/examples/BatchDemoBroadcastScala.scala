23bcd33511e46ee5af50a397dc48d3be33546597
yennanliu <f339339@gmail.com>
2020-12-06 11:26:55 +0800

    fix BatchDemoBroadcastScala.scala

--- src/main/scala/examples/BatchDemoBroadcastScala.scala
+++ src/main/scala/examples/BatchDemoBroadcastScala.scala
@@ -1,6 +1,5 @@
 package examples
 
-import com.sun.tools.javac.util.ListBuffer
 import org.apache.flink.api.common.functions.RichMapFunction
 import org.apache.flink.api.scala.ExecutionEnvironment
 import org.apache.flink.configuration.Configuration
@@ -27,6 +26,29 @@ object BatchDemoBroadcastScala extends App{
 
   val text = env.fromElements("zs", "ls", "ww")
 
-  // TBC
+  val result = text.map( new RichMapFunction[String, String] {
 
+    var listData: java.util.List[Map[String, Int]] = null
+    var allMap = Map[String, Int]()
+
+    override def open(parameters: Configuration): Unit = {
+      super.open(parameters)
+
+      // get broadcast data
+      this.listData = getRuntimeContext.getBroadcastVariable[Map[String, Int]]("broadcastMapName")
+
+      val it = listData.iterator()
+      while (it.hasNext) {
+        val next = it.next()
+        allMap = allMap.++(next)
+      }
+    }
+
+    override def map(value: String): String = {
+      val age = allMap.get(value).get
+      value+","+age
+    }
+    }).withBroadcastSet (toBroadcastData, "broadcastMapName") // run the op for broadcast data
+
+    println(result)
 }
