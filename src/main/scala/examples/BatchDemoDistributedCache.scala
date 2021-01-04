8bf9a42ecdb20de9c2e34ebba934954d80597391
yennanliu <f339339@gmail.com>
2021-01-05 07:51:44 +0800

    add examples/BatchDemoDistributedCache.scala

---
+++ src/main/scala/examples/BatchDemoDistributedCache.scala
@@ -0,0 +1,45 @@
+package examples
+
+import org.apache.commons.io.FileUtils
+import org.apache.flink.api.common.functions.RichMapFunction
+import org.apache.flink.api.scala.ExecutionEnvironment
+import org.apache.flink.configuration.Configuration
+
+object BatchDemoDistributedCache extends App{
+
+  println("BatchDemoDistributedCache run ...")
+
+  val env = ExecutionEnvironment.getExecutionEnvironment
+
+  import org.apache.flink.api.scala._
+
+  // register the doc
+  env.registerCachedFile("data/test.txt", "b.txt")
+
+  val data = env.fromElements("a", "b", "c", "d")
+
+  val result = data.map(new RichMapFunction[String, String] {
+
+    override def open(parameters: Configuration): Unit = {
+      
+      super.open(parameters)
+
+      // use the doc
+      val myFile = getRuntimeContext.getDistributedCache.getFile("b.txt")
+
+      val lines = FileUtils.readLines(myFile)
+
+      val it = lines.iterator()
+
+      while (it.hasNext){
+        val line = it.next();
+        println(s"line : " +line)
+      }
+    }
+    override def map(value: String) = {
+      value
+    }
+  })
+
+  result.print()
+}
