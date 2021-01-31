3861a9e198354d6e5c5da1a883edfd0683eed67a
yennanliu <f339339@gmail.com>
2021-01-31 13:25:55 +0800

    update

---
+++ java/src/main/java/src/main/java/dev/BatchJob.java
@@ -0,0 +1,17 @@
+//package src.main.java.dev;
+//
+//import org.apache.flink.api.java.ExecutionEnvironment;
+////import org.apache.flink.api.java.functions.
+//
+//
+//public class BatchJob {
+//
+//    public static void main(String[] args) throws Exception {
+//
+//        System.out.println("BatchJob run ...");
+//
+//        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+//
+//        env.execute("Flink Batch Java API Skeleton");
+//    }
+//}
