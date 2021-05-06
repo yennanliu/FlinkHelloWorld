5b3bde782a91500d139adf2c0584f53621bc4e79
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-05-06 18:41:34 +0800

    implement save-to-HDFS (streamKafkaToHDFSV1.scala

--- src/main/scala/streamKafkaToHDFS/streamKafkaToHDFSV1.scala
+++ src/main/scala/streamKafkaToHDFS/streamKafkaToHDFSV1.scala
@@ -1,8 +1,12 @@
 package streamKafkaToHDFS
 
 import java.util.Properties
+import java.util.concurrent.TimeUnit
 
-import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.api.common.serialization.{SimpleStringEncoder, SimpleStringSchema}
+import org.apache.flink.core.fs.Path
+import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink
+import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
 import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment, _}
@@ -29,6 +33,18 @@ object streamKafkaToHDFSV1 extends App {
   stream.print()
 
   // save to HDFS
+  val outputPath = "streamKafkaToHDFSV1"
+  val sink: StreamingFileSink[String] = StreamingFileSink
+    .forRowFormat(new Path(outputPath), new SimpleStringEncoder[String]("UTF-8"))
+    .withRollingPolicy(
+      DefaultRollingPolicy.builder()
+        .withRolloverInterval(TimeUnit.MINUTES.toMillis(15))
+        .withInactivityInterval(TimeUnit.MINUTES.toMillis(5))
+        .withMaxPartSize(1024 * 1024 * 1024)
+        .build())
+    .build()
+
+  stream.addSink(sink)
 
   env.execute("downstream-ins_jpw1a")
 }
